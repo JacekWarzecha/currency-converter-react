16eb7a84fa7354654907d3c92fb9dded584f7a29
JacekWarzecha <drjackys@gmail.com>
2023-02-21 10:40:10 +0100

    style.css in Result changed to styled.js

--- src/App/FormCantor/Result/index.js
+++ src/App/FormCantor/Result/index.js
@@ -1,20 +1,14 @@
-import "./style.css";
+import { StyledResult, TargetAmount } from "./styled";
 
 export const Result = ({ result }) => (
-  <p className="result">
+  <StyledResult>
     {result !== undefined && (
       <>
         {result.sourceAmount.toFixed(2)} PLN ={" "}
-        <strong className="result__targetAmount">
-          {result.targetAmount.toFixed(2)}
-          {result.currency}
-        </strong>
+        <TargetAmount>
+          {result.targetAmount.toFixed(2)} {result.currency}
+        </TargetAmount>
       </>
-      // ciekawi mminie skąd tutaj {result.currency} bierze się wyświetlanie tak naprawdę
-      // short, skrót waluty, a tego nie napisaliśmy że np. result.currency.short..
-      // chyba stąd, że currency ma value currency.short?
-      // a jeśli tak to dlaczego domyślnie jest 'dolar amerykański' wyswietlany,
-      // zamiast USD ?
     )}
-  </p>
+  </StyledResult>
 );
---
+++ src/App/FormCantor/Result/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledResult = styled.p`
+  text-align: center;
+`;
+
+export const TargetAmount = styled.strong`
+  border: 1px solid currentColor;
+  border-radius: 8px;
+  padding: 4px 10px;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
